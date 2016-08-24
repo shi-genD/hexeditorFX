a31f548a684604c3a5e228c793528a05e6da1f7b
shi <malishevsky.al@gmail.com>
2016-08-24 13:54:16 +0300

    added converting hex to string in different encoding

--- src/editor/view/ConverterOverviewController.java
+++ src/editor/view/ConverterOverviewController.java
@@ -1,13 +1,16 @@
 package editor.view;
 
-import editor.MainApp;
 import editor.util.HexByteConverter;
 import editor.util.IncorrectInputCodeException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by shi on 17.08.16.
@@ -19,6 +22,18 @@ public class ConverterOverviewController {
 
     @FXML
     private TextArea outputTextField;
+
+    @FXML
+    private RadioButton utf8Encoding;
+
+    @FXML
+    private RadioButton utf16Encoding;
+
+    @FXML
+    private RadioButton win1251Encoding;
+
+    @FXML
+    private RadioButton ansiEncoding;
 
     @FXML
     private RadioButton strToHex;
@@ -33,6 +48,16 @@ public class ConverterOverviewController {
 
     @FXML
     private void initialize() {
+        ToggleGroup rdGroup = new ToggleGroup();
+        utf8Encoding.setToggleGroup(rdGroup);
+        utf8Encoding.setUserData("UTF-8");
+        utf16Encoding.setToggleGroup(rdGroup);
+        utf16Encoding.setUserData("UTF-16");
+        win1251Encoding.setToggleGroup(rdGroup);
+        win1251Encoding.setUserData("Cp1251");
+        ansiEncoding.setToggleGroup(rdGroup);
+        ansiEncoding.setUserData("Cp1252");
+
         inputTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             if (strToHex.isSelected()) {
                 StringBuffer buf = new StringBuffer();
@@ -41,24 +66,46 @@ public class ConverterOverviewController {
                 }
                 outputTextField.setText(buf.toString());
             } else {
-                StringBuffer buf = new StringBuffer();
-                if (newValue.length()%2==0) {
-                    try {
-                        for (int i = 0; i < newValue.length(); i += 2) {
-                            buf.append((char)HexByteConverter.convertFromHex(newValue.substring(i, i + 2)));
-                        }
-                        outputTextField.setText(buf.toString());
-                    } catch (IncorrectInputCodeException e) {
-                        outputTextField.setText(e.getMessage());
-                    }
-                }
+                    hexToString(newValue, rdGroup.getSelectedToggle().getUserData().toString());
             }
+        });
+
+        hexToStr.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (hexToStr.isSelected()) {
+                utf8Encoding.setDisable(false);
+                utf16Encoding.setDisable(false);
+                win1251Encoding.setDisable(false);
+                ansiEncoding.setDisable(false);
+            } else {
+                utf8Encoding.setDisable(true);
+                utf16Encoding.setDisable(true);
+                win1251Encoding.setDisable(true);
+                ansiEncoding.setDisable(true);
+            }
+        });
+
+        rdGroup.selectedToggleProperty().addListener((observable, oldToggle, newToggle) -> {
+            hexToString(inputTextField.getText(), rdGroup.getSelectedToggle().getUserData().toString());
         });
     }
 
+    private void hexToString(String string, String encoing) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        if (string.length()%2==0) {
+            try {
+                for (int i = 0; i < string.length(); i += 2) {
+                    baos.write(HexByteConverter.convertFromHex(string.substring(i, i + 2)));
+                }
+                outputTextField.setText(baos.toString(encoing));
+            } catch (IncorrectInputCodeException | UnsupportedEncodingException e) {
+                outputTextField.setText(e.getMessage());
+            }
+        }
+    }
+
     public void radioSelect(ActionEvent event) {
-        inputTextField.deleteText(0, inputTextField.getText().length());
-        outputTextField.deleteText(0, outputTextField.getText().length());
+        inputTextField.setText("");
+        outputTextField.setText("");
     }
 
     public void setDialogStage(Stage stage) {
